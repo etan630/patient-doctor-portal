0532b4686b767af58c936238e59c246b1fc358f5
etan630 <erin_tan@outlook.com>
2024-07-24 08:42:42 -0400

    base structure of the website. has a login, register, and home page need to do html designs still

---
+++ server.js
@@ -0,0 +1,20 @@
+// Important Libraries
+const express = require("express")
+const app = express()
+
+
+// Routes
+app.get('/', (req, res) =>{
+    res.render("index.ejs")
+})
+
+app.get('/login', (req, res) => {
+    res.render("login.ejs")
+})
+
+app.get('/register', (req, res) => {
+    res.render("register.ejs")
+})
+// End Routes
+
+app.listen(3000)
